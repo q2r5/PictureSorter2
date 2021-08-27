852972ef69eda7ff8d78891850038bfdd73fced3
Callie Dunn <650804+q2r5@users.noreply.github.com>
2021-08-26 23:31:08 -0400

    View Models??? (v0.0.3)

--- App1/App1/MainWindow.xaml.cs
+++ App1/App1/MainWindow.xaml.cs
@@ -1,19 +1,11 @@
 ﻿using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Media.Imaging;
 using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
 using System.Runtime.InteropServices;
-using System.Threading.Tasks;
 using Windows.Storage;
-using Windows.Storage.AccessCache;
 using Windows.Storage.Pickers;
-using Windows.Storage.Search;
-using Windows.Graphics.Imaging;
-using Windows.Storage.Streams;
 using Microsoft.UI.Xaml.Input;
+using System.Linq;
 
 #if !UNIVERSAL
 using WinRT;
@@ -35,51 +27,13 @@ namespace App1
     /// </summary>
     public sealed partial class MainWindow : Window
     {
-        private StorageFolder CurrentFolder
-        {
-            get => _currentFolder;
-            set
-            {
-                if (_currentFolder != value)
-                {
-                    appData.Values["path"] = value.Path;
-                    _currentFolder = value;
-                    FolderChanged();
-                }
-            }
-        }
-
-        private StorageFolder _currentFolder;
-        private bool FilterChanged;
-        private HashSet<string> FilteredFileTypes
-        {
-            get => _filteredFileTypes;
-            set
-            {
-                if (_filteredFileTypes != value)
-                {
-                    appData.Values["fileTypes"] = string.Join(",", value);
-                    _filteredFileTypes = value;
-                    FilterChanged = true;
-                }
-            }
-        }
-        private HashSet<string> _filteredFileTypes;
-        private ObservableCollection<StorageFile> files;
-        private ObservableCollection<StorageFolder> folders;
-        private StorageFile currentFile;
-
-        private readonly ApplicationDataContainer appData = ApplicationData.Current.LocalSettings;
-
-        private (StorageFile, StorageFolder) lastCommand;
+        private readonly MainViewModel viewModel = MainViewModel.Instance;
 
         public MainWindow()
         {
             InitializeComponent();
 
-            FilteredFileTypes = ((string)appData.Values["fileTypes"] ?? ".jpg,.png,.gif").Split(",").ToHashSet();
-            FilterChanged = false;
-            foreach (string fileType in FilteredFileTypes)
+            foreach (string fileType in viewModel.FilteredFileTypes)
             {
                 foreach (MenuFlyoutItemBase item in FilterMenu.Items)
                 {
@@ -90,12 +44,10 @@ namespace App1
                 }
             }
 
-            if (appData.Values["path"] != null)
+            if (viewModel.CurrentFolder != null)
             {
-                CurrentFolder = StorageFolder.GetFolderFromPathAsync((string)appData.Values["path"]).GetAwaiter().GetResult();
+                FolderChanged();
             }
-
-            _ = fileList.Focus(FocusState.Programmatic);
         }
 
         private async void PickFolderButton_Click(object sender, RoutedEventArgs e)
@@ -105,7 +57,7 @@ namespace App1
                 SuggestedStartLocation = PickerLocationId.Desktop
             };
 
-            foreach (string fileType in FilteredFileTypes)
+            foreach (string fileType in viewModel.FilteredFileTypes)
             {
                 folderPicker.FileTypeFilter.Add(fileType);
             }
@@ -120,76 +72,22 @@ namespace App1
             }
 #endif
 
-            CurrentFolder = await folderPicker.PickSingleFolderAsync();
+            viewModel.CurrentFolder = await folderPicker.PickSingleFolderAsync();
         }
 
-        private async void FolderChanged()
+        private void FolderChanged()
         {
-            if (CurrentFolder == null) { return; }
-
-            StorageApplicationPermissions.FutureAccessList.AddOrReplace("PickedFolderToken", CurrentFolder);
-
-            files = await GetFiles(CurrentFolder);
-            folders = await GetFolders(CurrentFolder);
-
-            MoveButton.IsEnabled = true;
             RefreshButton.IsEnabled = true;
             ConvertButton.IsEnabled = true;
             NewCategoryButton.IsEnabled = true;
-
-            fileList.ItemsSource = files;
-            fileList.SelectedIndex = 0;
-
-            FolderList.ItemsSource = folders;
-            FolderList.DisplayMemberPath = "Name";
-            FolderList.SelectedIndex = 0;
-
-            CategoryGrid.ItemsSource = folders;
-
-            FolderHeader.Text = CurrentFolder.DisplayName;
-        }
-
-        private async Task<ObservableCollection<StorageFile>> GetFiles(StorageFolder folder)
-        {
-            QueryOptions query = new(CommonFileQuery.OrderByName, fileTypeFilter: FilteredFileTypes);
-            query.FolderDepth = FolderDepth.Shallow;
-            query.IndexerOption = IndexerOption.UseIndexerWhenAvailable;
-            StorageFileQueryResult result = folder.CreateFileQueryWithOptions(query);
-            IReadOnlyList<StorageFile> files = await result.GetFilesAsync();
-
-            ObservableCollection<StorageFile> fileList = new();
-
-            for (int i = 0; i < files.Count; i += 1)
-            {
-                fileList.Add(files[i]);
-            }
-
-            return fileList;
-        }
-
-        private static async Task<ObservableCollection<StorageFolder>> GetFolders(StorageFolder folder)
-        {
-            StorageFolderQueryResult result = folder.CreateFolderQuery();
-            IReadOnlyList<StorageFolder> folders = await result.GetFoldersAsync();
-            ObservableCollection<StorageFolder> folderList = new();
-            for (int i = 0; i < folders.Count; i += 1)
-            {
-                folderList.Add(folders[i]);
-            }
-
-            return folderList;
         }
 
         private void FileList_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             if (fileList.SelectedIndex == -1) { return; }
 
-            currentFile = files[fileList.SelectedIndex];
-            Uri uri = new(currentFile.Path);
-            BitmapImage image = new(uri);
-            ImagePreview.Source = image;
-
-            string fileType = currentFile.FileType.ToUpperInvariant().Replace(".", "");
+            viewModel.CurrentFile = viewModel.CurrentFiles.ElementAt(fileList.SelectedIndex);
+            string fileType = viewModel.CurrentFile.FileType.ToUpperInvariant().Replace(".", "");
             if (fileType == "JPG") { fileType = "JPEG"; }
             else if (fileType == "WDP") { fileType = "JXR"; }
 
@@ -202,98 +100,30 @@ namespace App1
             }
         }
 
-        private async void MoveButton_Click(object sender, RoutedEventArgs e)
+        private void MoveButton_Click(object sender, RoutedEventArgs e)
         {
-            if (fileList.SelectedIndex == -1 || FolderList.SelectedIndex == -1) { return; }
+            //if (fileList.SelectedIndex == -1 || FolderList.SelectedIndex == -1) { return; }
 
-            StorageFolder folder = folders[FolderList.SelectedIndex];
+            //StorageFolder folder = folders[FolderList.SelectedIndex];
 
-            await MoveFile(currentFile, folder);
+            //await MoveFile(currentFile, folder);
         }
 
-        private async void RefreshFilesButton_Click(object sender, RoutedEventArgs e)
+        private void RefreshFilesButton_Click(object sender, RoutedEventArgs e)
         {
-            files = await GetFiles(CurrentFolder);
-            fileList.ItemsSource = files;
-            fileList.SelectedIndex = 0;
+            viewModel.GetFiles(viewModel.CurrentFolder);
         }
 
-        private async void ConvertOption_Click(object sender, RoutedEventArgs e)
+        private void ConvertOption_Click(object sender, RoutedEventArgs e)
         {
             string option = ((MenuFlyoutItem)sender).Tag.ToString();
-            string extension = "";
-            Guid encoderID = new();
-
-            if (option == "PNG")
-            {
-                encoderID = BitmapEncoder.PngEncoderId;
-                extension = ".png";
-            }
-            else if (option == "JPEG")
-            {
-                encoderID = BitmapEncoder.JpegEncoderId;
-                extension = ".jpg";
-            }
-            else if (option == "GIF")
-            {
-                encoderID = BitmapEncoder.GifEncoderId;
-                extension = ".gif";
-            }
-            else if (option == "HEIF")
-            {
-                encoderID = BitmapEncoder.HeifEncoderId;
-                extension = ".heif";
-            } else if (option == "JXR")
-            {
-                encoderID = BitmapEncoder.JpegXREncoderId;
-                extension = ".jxr";
-            }
-
-            BitmapDecoder decoder = await BitmapDecoder.CreateAsync(await currentFile.OpenReadAsync());
-            SoftwareBitmap bitmap = await decoder.GetSoftwareBitmapAsync();
-
-            await currentFile.RenameAsync(currentFile.DisplayName + ".bak", NameCollisionOption.GenerateUniqueName);
-            StorageFile newFile = await CurrentFolder.CreateFileAsync(currentFile.DisplayName.Split(".")[0] + extension);
-
-            using IRandomAccessStream stream = await newFile.OpenAsync(FileAccessMode.ReadWrite);
-            BitmapEncoder encoder = await BitmapEncoder.CreateAsync(encoderID, stream);
-            encoder.SetSoftwareBitmap(bitmap);
-            encoder.IsThumbnailGenerated = true;
-
-            try
-            {
-                await encoder.FlushAsync();
-            }
-            catch (Exception err)
-            {
-                const int WINCODEC_ERR_UNSUPPORTED_OPERATION = unchecked((int)0x88982F81);
-                switch (err.HResult)
-                {
-                    case WINCODEC_ERR_UNSUPPORTED_OPERATION:
-                        encoder.IsThumbnailGenerated = false;
-                        break;
-                    default:
-                        throw;
-                }
-            }
-
-            if (encoder.IsThumbnailGenerated == false)
-            {
-                await encoder.FlushAsync();
-            }
+            viewModel.ConvertFile(viewModel.CurrentFile, option);
         }
 
-        private async void NewCategoryButton_Click(object sender, RoutedEventArgs e)
+        private void NewCategoryButton_Click(object sender, RoutedEventArgs e)
         {
             NewCategoryButton.Flyout.Hide();
-            _ = await CurrentFolder.CreateFolderAsync(NewCatName.Text, CreationCollisionOption.GenerateUniqueName);
-            folders = await GetFolders(CurrentFolder);
-
-            FolderList.ItemsSource = folders;
-            FolderList.DisplayMemberPath = "Name";
-            FolderList.SelectedIndex = 0;
-
-            CategoryGrid.ItemsSource = folders;
+            viewModel.NewFolder(NewCatName.Text, viewModel.CurrentFolder);
         }
 
         private void NewCatName_TextChanged(object sender, TextChangedEventArgs e)
@@ -301,32 +131,15 @@ namespace App1
             NewCatAddButton.IsEnabled = NewCatName.Text.Length != 0;
         }
 
-        private async void CategoryGrid_ItemClick(object sender, ItemClickEventArgs e)
+        private void CategoryGrid_ItemClick(object sender, ItemClickEventArgs e)
         {
-            await MoveFile(currentFile, e.ClickedItem as StorageFolder);
+            viewModel.MoveFile(viewModel.CurrentFile, e.ClickedItem as StorageFolder);
+            UndoButton.IsEnabled = true;
         }
 
-        private async Task MoveFile(StorageFile file, StorageFolder folder)
+        private void RefreshCategoriesButton_Click(object sender, RoutedEventArgs e)
         {
-            if (file == null || folder == null || fileList.SelectedIndex == -1) { return; }
-            int selectedIndex = fileList.SelectedIndex;
-            await file.MoveAsync(folder);
-            _ = files.Remove(file);
-            fileList.SelectedIndex = selectedIndex;
-            lastCommand = (file, folder);
-            if (UndoButton.IsEnabled) { UndoButton.IsEnabled = false; RedoButton.IsEnabled = true; }
-            else { UndoButton.IsEnabled = true; RedoButton.IsEnabled = false; }
-        }
-
-        private async void RefreshCategoriesButton_Click(object sender, RoutedEventArgs e)
-        {
-            folders = await GetFolders(CurrentFolder);
-
-            FolderList.ItemsSource = folders;
-            FolderList.DisplayMemberPath = "Name";
-            FolderList.SelectedIndex = 0;
-
-            CategoryGrid.ItemsSource = folders;
+            viewModel.GetFolders(viewModel.CurrentFolder);
         }
 
         private void ToggleMenuFlyoutItem_Click(object sender, RoutedEventArgs e)
@@ -334,34 +147,33 @@ namespace App1
             ToggleMenuFlyoutItem item = sender as ToggleMenuFlyoutItem;
             string optionTag = item.Tag.ToString();
 
-            _ = item.IsChecked ? FilteredFileTypes.Add(optionTag) : FilteredFileTypes.Remove(optionTag);
+            _ = item.IsChecked ? viewModel.FilteredFileTypes.Add(optionTag) : viewModel.FilteredFileTypes.Remove(optionTag);
         }
 
-        private async void FilterMenu_Closed(object sender, object e)
+        private void FilterMenu_Closed(object sender, object e)
         {
-            if (FilterChanged == false) { return; }
-            files = await GetFiles(CurrentFolder);
-            fileList.ItemsSource = files;
-            fileList.SelectedIndex = 0;
+            //if (FilterChanged == false) { return; }
+            //files = await GetFiles(CurrentFolder);
+            //fileList.ItemsSource = files;
+            //fileList.SelectedIndex = 0;
         }
 
-        private async void Undo(object sender, RoutedEventArgs e)
+        private void Undo(object sender, RoutedEventArgs e)
         {
-            if (files.Contains(lastCommand.Item1)) { return; }
-            await MoveFile(lastCommand.Item1, CurrentFolder);
             int index = fileList.SelectedIndex;
-            files.Insert(index, lastCommand.Item1);
-            fileList.ItemsSource = files;
+            viewModel.GetFiles(viewModel.CurrentFolder);
             fileList.SelectedIndex = index;
+            UndoButton.IsEnabled = false;
+            RedoButton.IsEnabled = true;
         }
 
-        private async void Redo(object sender, RoutedEventArgs e)
+        private void Redo(object sender, RoutedEventArgs e)
         {
-            await MoveFile(lastCommand.Item1, lastCommand.Item2);
-            files = await GetFiles(CurrentFolder);
             int index = fileList.SelectedIndex;
-            fileList.ItemsSource = files;
+            viewModel.Redo();
             fileList.SelectedIndex = index;
+            UndoButton.IsEnabled = true;
+            RedoButton.IsEnabled = false;
         }
 
         private void NewCatName_KeyDown(object sender, KeyRoutedEventArgs e)
@@ -370,6 +182,29 @@ namespace App1
             {
                 NewCategoryButton_Click(sender, e);
             }
+        }
+
+        private void PathBox_KeyDown(object sender, KeyRoutedEventArgs e)
+        {
+            if (e.Key == Windows.System.VirtualKey.Enter)
+            {
+                PathButton_Click(sender, e);
+            }
+        }
+
+        private void PathButton_Click(object sender, RoutedEventArgs e)
+        {
+            FolderHeader.ContextFlyout.Hide();
+            viewModel.SetFolder(PathBox.Text);
+            if (viewModel.CurrentFolder != null)
+            {
+                FolderChanged();
+            }
+        }
+
+        private void PathBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            PathButton.IsEnabled = PathBox.Text.Length != 0;
         }
     }
 }
--- App1/App1/ViewModel.cs
+++ App1/App1/ViewModel.cs
@@ -1,17 +1,22 @@
-﻿using System;
+﻿using Microsoft.UI.Xaml.Media.Imaging;
+using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Windows.Graphics.Imaging;
 using Windows.Storage;
 using Windows.Storage.AccessCache;
 using Windows.Storage.Search;
+using Windows.Storage.Streams;
+using System.Runtime.CompilerServices;
+using System.Collections.ObjectModel;
 
 namespace App1
 {
-    class MainViewModel
+    public class MainViewModel : INotifyPropertyChanged
     {
+        public static MainViewModel Instance { get; private set; }
+
         private readonly ApplicationDataContainer appData = ApplicationData.Current.LocalSettings;
 
         public StorageFolder CurrentFolder
@@ -19,71 +24,215 @@ namespace App1
             get => _currentFolder;
             set
             {
-                if (_currentFolder != value)
+                if (SetProperty(ref _currentFolder, value))
                 {
+                    StorageApplicationPermissions.FutureAccessList.AddOrReplace("PickedFolderToken", value);
                     appData.Values["path"] = value.Path;
-                    _currentFolder = value;
-                    ChangeFolderAsync();
+                    GetFiles(value);
+                    GetFolders(value);
                 }
             }
         }
         private StorageFolder _currentFolder;
 
-        private HashSet<string> FilteredFileTypes
+        public StorageFile CurrentFile
+        {
+            get => _currentFile;
+            set => SetProperty(ref _currentFile, value);
+
+        }
+        private StorageFile _currentFile;
+
+        public ObservableCollection<StorageFile> CurrentFiles
+        {
+            get => _currentFiles;
+            set => SetProperty(ref _currentFiles, value);
+        }
+        private ObservableCollection<StorageFile> _currentFiles = new();
+
+        public HashSet<StorageFolder> Categories
+        {
+            get => _categories;
+            set => SetProperty(ref _categories, value);
+        }
+        private HashSet<StorageFolder> _categories = new();
+
+        public HashSet<string> FilteredFileTypes
         {
             get => _filteredFileTypes;
             set
             {
-                if (_filteredFileTypes != value)
+                if (SetProperty(ref _filteredFileTypes, value))
                 {
                     appData.Values["fileTypes"] = string.Join(",", value);
-                    _filteredFileTypes = value;
                 }
             }
         }
-        private HashSet<string> _filteredFileTypes;
+        private HashSet<string> _filteredFileTypes = new();
 
-        private ObservableCollection<StorageFile> files;
-        private ObservableCollection<StorageFolder> folders;
-        private StorageFile currentFile;
+        private (StorageFile, StorageFolder) lastCommand;
 
-        public async Task ChangeFolderAsync()
+        static MainViewModel()
         {
-            StorageApplicationPermissions.FutureAccessList.AddOrReplace("PickedFolderToken", CurrentFolder);
-            files = await GetFiles(CurrentFolder);
-            folders = await GetFolders(CurrentFolder);
+            Instance = new MainViewModel();
         }
 
-        private async Task<ObservableCollection<StorageFile>> GetFiles(StorageFolder folder)
+        private MainViewModel()
         {
-            QueryOptions query = new(CommonFileQuery.OrderByName, fileTypeFilter: FilteredFileTypes);
-            query.FolderDepth = FolderDepth.Shallow;
-            query.IndexerOption = IndexerOption.UseIndexerWhenAvailable;
+            FilteredFileTypes = ((string)appData.Values["fileTypes"] ?? ".jpg,.png,.gif").Split(",").ToHashSet();
+            SetFolder(appData.Values["path"] as string);
+        }
+
+        public async void SetFolder(string path)
+        {
+            if (path == null) { return; }
+            CurrentFolder = await StorageFolder.GetFolderFromPathAsync(path);
+        }
+
+        public async void GetFiles(StorageFolder folder)
+        {
+            CurrentFolder = folder;
+            QueryOptions query = new(CommonFileQuery.OrderByName, fileTypeFilter: FilteredFileTypes)
+            {
+                FolderDepth = FolderDepth.Shallow,
+                IndexerOption = IndexerOption.UseIndexerWhenAvailable
+            };
             StorageFileQueryResult result = folder.CreateFileQueryWithOptions(query);
             IReadOnlyList<StorageFile> files = await result.GetFilesAsync();
-
-            ObservableCollection<StorageFile> fileList = new();
-
-            for (int i = 0; i < files.Count; i += 1)
+            if (CurrentFiles == null)
             {
-                fileList.Add(files[i]);
+                CurrentFiles = new(files);
+                return;
             }
-
-            return fileList;
+            else
+            {
+                if (CurrentFiles.Count > 0)
+                {
+                    CurrentFiles.Clear();
+                }
+                files.ToList().ForEach(f => CurrentFiles.Add(f));
+            }
         }
 
-        private static async Task<ObservableCollection<StorageFolder>> GetFolders(StorageFolder folder)
+        public async void GetFolders(StorageFolder folder)
         {
-            StorageFolderQueryResult result = folder.CreateFolderQuery();
-            IReadOnlyList<StorageFolder> folders = await result.GetFoldersAsync();
-            ObservableCollection<StorageFolder> folderList = new();
-            for (int i = 0; i < folders.Count; i += 1)
-            {
-                folderList.Add(folders[i]);
-            }
-
-            return folderList;
+            IReadOnlyList<StorageFolder> folders = await folder.GetFoldersAsync();
+            Categories = folders.ToHashSet();
         }
 
+        public async void MoveFile(StorageFile file, StorageFolder folder)
+        {
+            if (file == null || folder == null) { return; }
+            await file.MoveAsync(folder, file.Name, NameCollisionOption.GenerateUniqueName);
+            _ = CurrentFiles.Remove(file);
+
+            lastCommand = (file, folder);
+        }
+
+        public async void NewFolder(string name, StorageFolder folder)
+        {
+            if (name == null || folder == null) { return; }
+            _ = await folder.CreateFolderAsync(name);
+            GetFolders(folder);
+        }
+
+        public async void ConvertFile(StorageFile file, string fileType)
+        {
+            Guid encoderID = new();
+            string extension = "";
+
+            if (fileType == "PNG")
+            {
+                encoderID = BitmapEncoder.PngEncoderId;
+                extension = ".png";
+            }
+            else if (fileType == "JPEG")
+            {
+                encoderID = BitmapEncoder.JpegEncoderId;
+                extension = ".jpg";
+            }
+            else if (fileType == "GIF")
+            {
+                encoderID = BitmapEncoder.GifEncoderId;
+                extension = ".gif";
+            }
+            else if (fileType == "HEIF")
+            {
+                encoderID = BitmapEncoder.HeifEncoderId;
+                extension = ".heif";
+            }
+            else if (fileType == "JXR")
+            {
+                encoderID = BitmapEncoder.JpegXREncoderId;
+                extension = ".jxr";
+            }
+
+            BitmapDecoder decoder = await BitmapDecoder.CreateAsync(await file.OpenReadAsync());
+            SoftwareBitmap bitmap = await decoder.GetSoftwareBitmapAsync();
+
+            await file.RenameAsync(file.DisplayName + ".bak", NameCollisionOption.GenerateUniqueName);
+            StorageFile newFile = await CurrentFolder.CreateFileAsync(file.DisplayName.Split(".")[0] + extension);
+
+            IRandomAccessStream stream = await newFile.OpenAsync(FileAccessMode.ReadWrite);
+            BitmapEncoder encoder = await BitmapEncoder.CreateAsync(encoderID, stream);
+            encoder.SetSoftwareBitmap(bitmap);
+            encoder.IsThumbnailGenerated = true;
+
+            try
+            {
+                await encoder.FlushAsync();
+            }
+            catch (Exception err)
+            {
+                const int WINCODEC_ERR_UNSUPPORTED_OPERATION = unchecked((int)0x88982F81);
+                switch (err.HResult)
+                {
+                    case WINCODEC_ERR_UNSUPPORTED_OPERATION:
+                        encoder.IsThumbnailGenerated = false;
+                        break;
+                    default:
+                        throw;
+                }
+            }
+
+            if (encoder.IsThumbnailGenerated == false)
+            {
+                await encoder.FlushAsync();
+            }
+        }
+
+        public void Undo()
+        {
+            if (CurrentFiles.Contains(lastCommand.Item1)) { return; }
+            MoveFile(lastCommand.Item1, CurrentFolder);
+        }
+
+        public void Redo()
+        {
+            MoveFile(lastCommand.Item1, lastCommand.Item2);
+            GetFiles(CurrentFolder);
+
+        }
+
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+
+        protected bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (Equals(storage, value))
+            {
+                return false;
+            }
+
+            storage = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
     }
 }
